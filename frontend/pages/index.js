aaeb0f4da3b6235dee6aef181fbab46406d46f9b
Pham Mi <huynhmi1209@gmail.com>
2023-01-19 10:24:08 +0700

    add  config font-base in tailwind

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,7 +4,22 @@ import Slider from '../components/Slider';
 export default function Home() {
 	return (
 		<>
-			<span className="font-bold">Home</span>
+			<h1>Chocolate Chip Cookies h1</h1>
+			<h1>Contact</h1>
+			<h2>Crispy Croissants and Butter h2</h2>
+			<h2>Chocolate Heaven h2</h2>
+			<h3>ABOUT ME h3</h3>
+			<h4>Lucid Themes h4</h4>
+
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Phasellus consectetur metus aliquam lacus bibendum, non
+				sollicitudin diam dignissim. Suspendisse hendrerit ipsum eu
+				sapien tincidunt, vitae commodo dui laoreet. Nunc sed euismod
+				dui. Etiam placerat auctor posuere. Maecenas ac imperdiet lorem,
+				eget eleifend quam. Aenean facilisis scelerisque diam. Aenean
+				consectetur nisi sed laoreet scelerisque.
+			</p>
 		</>
 	);
 }
