fca3b70bc306cfd92f70ddd43b06706e0301b3ee
Pham Mi <huynhmi1209@gmail.com>
2023-01-25 11:58:27 +0700

    use button login

--- frontend/components/Layouts/Header/index.js
+++ frontend/components/Layouts/Header/index.js
@@ -16,7 +16,13 @@ function Header() {
 						<SocialLink />
 					</div>
 					<div className="border-l border-[rgba(255,255,255,0.5)] pl-5 ml-5 max-lg:hidden">
-						<User />
+						{/* <User /> */}
+						<Button
+							link="/"
+							rounded
+						>
+							Login
+						</Button>
 					</div>
 				</div>
 			</div>
