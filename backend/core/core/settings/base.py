b9573dc3b0ac7f75334b693e539722f7104799b8
Pham Mi <huynhmi1209@gmail.com>
2023-02-07 22:51:41 +0700

    cherry-pick  from be add Cloudinary

--- backend/core/core/settings/base.py
+++ backend/core/core/settings/base.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from datetime import timedelta
 from decouple import config
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
