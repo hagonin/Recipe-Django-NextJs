53ee605f3a0bcc61aaf03cd43ad388a6123671a6
Pham Mi <huynhmi1209@gmail.com>
2023-02-24 23:55:22 +0700

    add pagination btns

--- frontend/pages/recipes/index.js
+++ frontend/pages/recipes/index.js
@@ -1,4 +1,10 @@
+import WidgetLayout from '@components/Layouts/WidgetLayout';
+import Button from '@components/UI/Button';
 import Link from 'next/link';
+import {
+	HiOutlineChevronDoubleLeft,
+	HiOutlineChevronDoubleRight,
+} from 'react-icons/hi';
 
 function Recipe() {
 	return (
@@ -9,10 +15,19 @@ function Recipe() {
 				<Link href="/recipes/2">Item Recipe 2</Link>
 				<Link href="/recipes/3">Item Recipe 3</Link>
 			</div>
+			<div className="flex justify-between">
+				<Button icon={{ left: <HiOutlineChevronDoubleLeft /> }}>
+					Previous Recipe
+				</Button>
+				<Button icon={{ right: <HiOutlineChevronDoubleRight /> }}>
+					Next recipe
+				</Button>
+			</div>
 		</div>
 	);
 }
 
 export default Recipe;
 
-export const getStaticProps = () => {};
+Recipe.getLayout = (page) => <WidgetLayout>{page}</WidgetLayout>;
+// export const getStaticProps = () => {};
