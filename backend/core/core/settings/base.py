2fe90377ea06885913becce4458b7e99ddcb7eb6
gonin <goninha@outlook.fr>
2023-03-07 02:03:10 +0000

    change static to staticfiles templates

--- backend/core/core/settings/base.py
+++ backend/core/core/settings/base.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
