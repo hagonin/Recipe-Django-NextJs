b94c67378e948e681ed32c3cc523c38b0c2bcefd
gonin <goninha@outlook.fr>
2023-03-07 00:14:48 +0000

    remove conflicts in migrations

--- backend/core/core/settings/base.py
+++ backend/core/core/settings/base.py
@@ -55,8 +55,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
 
-CSRF_TRUSTED_ORIGINS = ["http://recipe-api.up.railway.app"]
-
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
         'Bearer': {
