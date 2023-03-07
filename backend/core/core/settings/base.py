b7a23b3e530435cabf4d29e52bea228ba3be4c33
gonin <goninha@outlook.fr>
2023-03-07 01:09:59 +0000

    add template admin site

--- backend/core/core/settings/base.py
+++ backend/core/core/settings/base.py
@@ -128,11 +128,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
