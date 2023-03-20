85976404bc52c85ad3346badf0c909c2e5185b14
gonin <goninha@outlook.fr>
2023-03-20 23:29:14 +0000

    add more UniCode for fraction

--- frontend/utils/convertFractionToUnicode.js
+++ frontend/utils/convertFractionToUnicode.js
@@ -5,14 +5,19 @@ function convertFractiontoUnicode(fraction) {
 	switch (true) {
 		case numerator === 1 && denominator === 2:
 			return String.fromCharCode(189);
+		case numerator === 1 && denominator === 3:
+			return String.fromCharCode(8531);
 		case numerator === 1 && denominator === 4:
 			return String.fromCharCode(188);
-		case numerator === 3 && denominator === 4:
-			return String.fromCharCode(190);
 		case numerator === 1 && denominator === 5:
 			return String.fromCharCode(8533);
+		case numerator === 2 && denominator === 3:
+			return String.fromCharCode(8532);
 		case numerator === 2 && denominator === 5:
 			return String.fromCharCode(8534);
+		case numerator === 3 && denominator === 4:
+			return String.fromCharCode(190);
+
 		default:
 			return fraction;
 	}
